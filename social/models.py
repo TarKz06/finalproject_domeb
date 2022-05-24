ec488dd3db43a939cf66c9fe20c1f85d00e893eb
TarKz06 <khematat_s@elearning.cmu.ac.th>
2022-05-24 18:24:05 +0700

    User follow neighbor for extension to dm (maybe i can)

--- social/models.py
+++ social/models.py
@@ -9,6 +9,8 @@ class Post(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name='likes')
+    dislikes = models.ManyToManyField(User, blank=True, related_name='dislikes')
 
 class Comment(models.Model):
  	comment = models.TextField()
@@ -23,6 +25,7 @@ class UserProfile(models.Model):
 	birth_date=models.DateField(null=True, blank=True)
 	location = models.CharField(max_length=100, blank=True, null=True)
 	picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/default.png', blank=True)
+	followers = models.ManyToManyField(User, blank=True, related_name='followers')
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
