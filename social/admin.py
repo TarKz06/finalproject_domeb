8032ba853a2153fca486013b16e73cbf6d666574
TarKz06 <khematat_s@elearning.cmu.ac.th>
2022-05-24 15:05:40 +0700

    User can edit your own profile (optional)

--- social/admin.py
+++ social/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Post
-from .models import UserProfile
-
+from .models import Post, UserProfile
 
 admin.site.register(Post)
 admin.site.register(UserProfile)
